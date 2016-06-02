6030ef5c4137378482d35a3c5aa438fd75ca5b06
guoyongfeng <296512521@qq.com>
2016-06-02 10:48:48 +0800

    9.发表文章

--- model/index.js
+++ model/index.js
@@ -8,10 +8,18 @@ var userSchema = new mongoose.Schema({
     email:{type:String}, //邮箱
     avatar:{type:String} //头像
 });
+var articleSchema = new mongoose.Schema({
+    title:{type:String},//标题
+    content:{type:String},//正文
+    createAt:{type:Date,default:new Date()}, //发表时间
+    user:{type:mongoose.Schema.Types.ObjectId,ref:'user'} //作者 ref代表这个ID引用的是哪个模型的ID
+});
 //3. 定义模型
 var userModel = mongoose.model('user',userSchema);
+var articleModel = mongoose.model('article',articleSchema);
 
 //导出用户模型
 module.exports = {
-    user:userModel
+    user:userModel,
+    article:articleModel
 }
--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var model = require('../model');
 //获取增加表单
 router.get('/add', function(req, res, next) {
   res.render('article/add',{});
 });
 //提交增加表单
 router.post('/add', function(req, res, next) {
-  res.send('提交增加文章的表单');
+  var article = req.body;//标题 正文
+  article.user = req.session.user._id;
+  model.article.create(article,function(err,doc){
+     if(err){
+       res.redirect('back');
+     }else{
+       res.redirect('/');
+     }
+  });
+
 });
 
 module.exports = router;
