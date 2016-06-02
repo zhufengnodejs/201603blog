ae836a2ca791528c358e247cd9fe6b23c2fcf693
guoyongfeng <296512521@qq.com>
2016-06-02 11:03:20 +0800

    10.显示文章列表

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
+var model = require('../model');
 //创建一个路由实例
 var router = express.Router();
 
 /* 当用户访问/路径的时候，执行此回调函数 */
 router.get('/', function(req, res, next) {
-  var user = req.session.user;
-  res.render('index', { title: '首页'});
+  //populate表示填充 把user从id转成对象
+  // {user:{}}
+  model.article.find().populate('user').exec(function(err,docs){
+    console.log(docs);
+    if(err){
+      res.render('index', { title: '首页',articles:[]});
+    }else{
+      res.render('index', { title: '首页',articles:docs});
+    }
+  });
+
 });
 
 module.exports = router;
